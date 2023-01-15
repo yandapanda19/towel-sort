a8bee0dfd482b5a2a0d5a3c31c65edeba10e2b39
yandapanda19 <i.am.kitiket@gmail.com>
2023-01-15 21:23:36 +0200

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+
+  if (matrix === undefined) {
+    return result;
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    if (Array.isArray(matrix[i])) {
+      if (i % 2 !== 0) {
+        result = result.concat(towelSort(matrix[i].reverse()));
+      } else {
+        result = result.concat(towelSort(matrix[i]));
+      }
+    } else {
+      result.push(matrix[i]);
+    }
+  }
+  return result;
 }
